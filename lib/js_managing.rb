84b852ac589f7fbdb6994158b0ee857de42a5939
Paul Keen <125715+pftg@users.noreply.github.com>
2024-10-07 21:13:05 +0200

    Initial project

---
+++ lib/js_managing.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "js_managing/version"
+
+module JsManaging
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/js_managing/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module JsManaging
+  VERSION = "0.1.0"
+end
